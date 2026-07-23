5095dd4b7925a7322290329e8f4ac873a87d9589
chngvit <166533354+Chngivt@users.noreply.github.com>
2026-07-23 21:13:15 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/banh-chung-banh-tet/', // Đúng tên repository trên link GitHub của bạn
+  base: './',
 })
